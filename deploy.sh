ee49e66a01095b073e3aabe22b9741efc5eba30d
Park <seonggyu@88665a322379.ant.amazon.com>
2021-12-20 22:45:39 +0900

    update auth on operations

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ echo "Start shell script file!"
 # if pidof -x "deploy.sh" > /dev/null; then
 #     echo "Process already running!"
 # fi
-npx kill-port 3000
+sudo npx kill-port 3000
 echo "Server is running!"
-npm start
+sudo npm start
 
 
