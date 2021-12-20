ccbe087a04f9e99d87af9eec03b8b37fc2051d6d
Park <seonggyu@88665a322379.ant.amazon.com>
2021-12-20 22:49:57 +0900

    update scripts

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Start shell script file!"
 # if pidof -x "deploy.sh" > /dev/null; then
 #     echo "Process already running!"
 # fi
+cd app
 sudo npx kill-port 3000
 echo "Server is running!"
 sudo npm start
