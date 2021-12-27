513b6f77b865e02e8fd224f22ea63e4b6b557133
Park <seonggyu@88665a322379.ant.amazon.com>
2021-12-27 14:47:49 +0900

    change path

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# deploy.sh
-echo "Setup path"
-export PATH=~/.local/bin:$PATH
-source ~/.profile
+REPOSITORY=/home/ec2-user/app
+
+# echo "Setup path"
+# export PATH=~/.local/bin:$PATH
+# source ~/.profile
 echo "Start shell script file!"
 # if pidof -x "deploy.sh" > /dev/null; then
 #     echo "Process already running!"
 # fi
-cd app
+cd $REPOSITORY
 npx kill-port 3000
 echo "Server is running!"
 npm start
